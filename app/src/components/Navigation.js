357d75b76671ff1a444c3bc6e53db4d1cab0de8e
Casey Key <caseykey@rams.colostate.edu>
2020-04-21 14:31:42 -0600

    use contexts, effects, and pass argument to cacheCall

--- app/src/components/Navigation.js
+++ app/src/components/Navigation.js
@@ -1,37 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { DrizzleContext } from "@drizzle/react-plugin";
+import { Contract } from "web3-eth-contract";
 
-export default ({drizzle, drizzleState}) => {
+export default () => {
+
+  const drizzleContext = useContext(DrizzleContext.Context);
   const [dataKey, setDataKey] = useState(null);
-  
+  const [group, setGroup] = useState(null);
+  const account = drizzleContext.drizzleState.accounts[0]; 
+
+  const web3 = drizzleContext.drizzle.web3;
+
   useEffect(() => {
-    const contract = drizzle.contracts.ComplexStorage;
+    if (drizzleContext.initialized) {
+      const contract = drizzleContext.drizzle.contracts.ComplexStorage;
+      const dataKey = contract.methods["userToGroup"].cacheCall(account);
+      setDataKey(dataKey);
+      console.log(dataKey);
+    }
+  }, [drizzleContext.initialized]);
 
-    // get the value of string1 from the contract
-    const dataKey = contract.methods["userToGroup"].cacheCall();
+  useEffect(() => {
+    if (dataKey) {
+      const ContractStore = drizzleContext.drizzleState.contracts.ComplexStorage;
+      // Use the saved 'dataKey' to get the return value from earlier.
+      setGroup(ContractStore.userToGroup[dataKey].value)
+      console.log(ContractStore.userToGroup);
+      console.log(group);
+    }
+  }, [drizzleContext.drizzleState]);
 
-    // Save the dataKey for later use
-    setDataKey(dataKey);
-  }, [drizzle.contracts.ComplexStorage]);
-
-  // Get the contract state
-  const ContractStore = drizzleState.contracts.ComplexStorage;
-
-  // Use the saved 'dataKey' to get the 'group' variable
-  const userToGroup = ContractStore.userToGroup[dataKey];
-
-  // Get currentaccount
-  const account = drizzleState.accounts[0];
-
-  const group = userToGroup(account);
 
   return (
     <div className="navbar">
-      <div className="group">
-        <h2>{account}'s Group: {group && drizzle.web3.utils.toUtf8(group.value)}</h2>
-      </div>
-      <div className="selector">
-
-      </div>
+        <h2>{account}'s Group: {group && group.name}</h2>
     </div>
   );
 };
