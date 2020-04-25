ede54cbd5e75d609043602ba622ff7394d493f7d
Casey Key <caseykey@rams.colostate.edu>
2020-04-25 00:30:46 -0600

    now displays users name instead of address

--- app/src/components/Navigation.js
+++ app/src/components/Navigation.js
@@ -5,8 +5,10 @@ import { Contract } from "web3-eth-contract";
 export default () => {
 
   const drizzleContext = useContext(DrizzleContext.Context);
-  const [dataKey, setDataKey] = useState(null);
+  const [groupsDataKey, setGroupsDataKey] = useState(null);
   const [groups, setGroups] = useState(null);
+  const [nameDataKey, setNameDataKey] = useState(null);
+  const [name, setName] = useState(null);
   const account = drizzleContext.drizzleState.accounts[0]; 
 
   const web3 = drizzleContext.drizzle.web3;
@@ -14,24 +16,31 @@ export default () => {
   useEffect(() => {
     if (drizzleContext.initialized) {
       const contract = drizzleContext.drizzle.contracts.PaymentHub;
-      const dataKey = contract.methods["userToGroups"].cacheCall(account, 0);
-      setDataKey(dataKey);
-      console.log(dataKey);
+
+      const groupsDataKey = contract.methods["userToGroups"].cacheCall(account, 0);
+      setGroupsDataKey(groupsDataKey);
+    
+      const nameDataKey = contract.methods["userToMember"].cacheCall(account);
+      setNameDataKey(nameDataKey);
     }
   }, [drizzleContext.initialized]);
 
   useEffect(() => {
-    if (dataKey) {
-      const ContractStore = drizzleContext.drizzleState.contracts.PaymentHub;
-      // Use the saved 'dataKey' to get the return value from earlier.
-      setGroups(ContractStore.userToGroups[dataKey].value)
+    const ContractStore = drizzleContext.drizzleState.contracts.PaymentHub;
+
+    // Use the saved 'dataKey' to get the return value from earlier.
+    if (groupsDataKey) {
+      setGroups(ContractStore.userToGroups[groupsDataKey].value);
+    }
+    if (nameDataKey && ContractStore.userToMember[nameDataKey]) {
+      setName(ContractStore.userToMember[nameDataKey].value);
     }
   }, [drizzleContext.drizzleState]);
 
   
   return (
     <div className="navbar">
-        <h2>{account}'s Group: {groups && groups.name}</h2>
+        <h2>{name && name.name}'s Group: {groups && groups.name}</h2>
     </div>
   );
 };
--- contracts/PaymentHub.sol
+++ contracts/PaymentHub.sol
@@ -5,6 +5,7 @@ contract PaymentHub {
     // Mapping for finding a user's groups
     mapping (address => Group[]) public userToGroups;
     mapping (address => int256) public userToBalance;
+    mapping (address => Member) public userToMember;
 
     Group[] public groups; // The contract stores all groups, serves as a hub. Various groups will not interact with each other
 
@@ -29,12 +30,15 @@ contract PaymentHub {
     function createGroup(string memory _groupName, string memory _groupOwnerName) public returns(uint) {
         uint groupID = groups.length++; // Must be compiled below 0.6 to increase length this way // Manually increase the groups array size
         Group storage group = groups[groups.length - 1];
-        group.name = _groupName; // Manually set the group name
+
         Member memory member = Member(_groupOwnerName, 0, msg.sender);
         group.friends.push(member); // Add the first member, which is the creator
+
+        group.name = _groupName; // Manually set the group name
         group.id = groupID;
 
         userToGroups[msg.sender].push(group);
+        userToMember[msg.sender] = member;
 
         return groupID;
     }
