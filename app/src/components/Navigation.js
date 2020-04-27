884ee0249574e0036b155387d6d97fbe1b093f12
Casey Key <caseykey@rams.colostate.edu>
2020-04-26 22:22:18 -0600

    added navbar from reactstrap

--- app/src/components/Navigation.js
+++ app/src/components/Navigation.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState, useContext } from "react";
 import { DrizzleContext } from "@drizzle/react-plugin";
 import { Contract } from "web3-eth-contract";
+import { Navbar, NavbarBrand, NavbarToggler, Nav, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap";
 
 export default () => {
 
+  const [toggle, setToggle] = useState(false);
+
   const drizzleContext = useContext(DrizzleContext.Context);
   const [groupsDataKey, setGroupsDataKey] = useState(null);
+  const [groupSizeDataKey, setGroupSizeDataKey] = useState(null);
   const [groups, setGroups] = useState(null);
   const [nameDataKey, setNameDataKey] = useState(null);
   const [name, setName] = useState(null);
@@ -19,6 +23,9 @@ export default () => {
 
       const groupsDataKey = contract.methods["userToGroups"].cacheCall(account, 0);
       setGroupsDataKey(groupsDataKey);
+
+      const groupSizeDataKey = contract.methods["getNumUserGroups"].cacheCall(account);
+      setGroupSizeDataKey(groupSizeDataKey);
     
       const nameDataKey = contract.methods["userToMember"].cacheCall(account);
       setNameDataKey(nameDataKey);
@@ -27,20 +34,40 @@ export default () => {
 
   useEffect(() => {
     const ContractStore = drizzleContext.drizzleState.contracts.PaymentHub;
-
+    console.log("yo");
+    console.log("aye", groupsDataKey);
+    console.log(ContractStore.userToGroups[groupsDataKey]);
     // Use the saved 'dataKey' to get the return value from earlier.
     if (groupsDataKey) {
+      console.log("before poop");
       setGroups(ContractStore.userToGroups[groupsDataKey].value);
+      console.log("poop");
+      console.log(groups);
     }
     if (nameDataKey && ContractStore.userToMember[nameDataKey]) {
       setName(ContractStore.userToMember[nameDataKey].value);
     }
   }, [drizzleContext.drizzleState]);
 
+  console.log("outside");
   
   return (
-    <div className="navbar">
-        <h2>{name && name.name}'s Group: {groups && groups.name}</h2>
+
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">{name && name.name}'s Group: {groups && groups.name}</NavbarBrand>
+          <Nav className="mr-auto" navbar>
+            <UncontrolledDropdown nav inNavbar className="clearfix">
+              <DropdownToggle nav caret className="float-right">
+                Groups
+              </DropdownToggle>
+              <DropdownMenu right>
+                {groups && groups.map}
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+      </Navbar>
+      <h2>{name && name.name}'s Group: {groups && groups.name}</h2>
     </div>
   );
 };
