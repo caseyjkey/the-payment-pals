214ed158844203d2a6c61e468fae30cbeb00cd96
Corbin Callahan <50761023+corbincallahan@users.noreply.github.com>
2020-04-30 21:46:25 -0600

    Add AddFriend to main page

--- app/src/PaymentPals.js
+++ app/src/PaymentPals.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Navigation from "./components/Navigation.js";
+import AddFriend from "./components/AddFriend.js";
 
 export default ({ drizzle, drizzleState }) => {
     // destructure drizzle and drizzleState from props
@@ -18,6 +19,7 @@ export default ({ drizzle, drizzleState }) => {
                 setGID={setGroupID}
             />
             <h2>Current GID: {groupID}</h2>
+            <AddFriend drizzle={drizzle} groupId={0} userAddress={0xaB32fa021cb7e72d2155497eB4e9De67fe09e035}/>
         </div>
     );
 };
