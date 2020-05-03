20d0e3259cde13ccc5b595b6da86615106059094
Casey Key <caseykey@rams.colostate.edu>
2020-05-02 21:15:06 -0600

    added buidler evm, compiled, added address to artifact

--- testExclude/TestSimpleStorage.sol
+++ testExclude/TestSimpleStorage.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.4.21 <0.7.0;
+/* pragma solidity >=0.4.21 <0.7.0;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
@@ -14,4 +14,4 @@ contract TestSimpleStorage {
 
         Assert.equal(simpleStorage.storedData(), expected, "It should store the value 89.");
     }
-}
+} */ 
