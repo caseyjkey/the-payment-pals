2b56f4f58a2d1178036cd88f31d9d9dc6bd1d1ef
Casey Key <caseykey@rams.colostate.edu>
2020-05-07 00:32:10 -0600

    Removed localhost as drizzle's custom provider

--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -2,10 +2,6 @@ import Web3 from "web3";
 import PaymentHub from "./contracts/PaymentHub.json";
 
 const options = {
-  web3: {
-    block: false,
-    customProvider: new Web3("ws://localhost:8545"),
-  },
   contracts: [PaymentHub],
   events: {},
 };
