be52290b05e9d75bd8d0484b4bd69daf90179ab7
Casey Key <caseykey@rams.colostate.edu>
2020-05-06 23:41:24 -0600

    added back welcome modal

--- app/src/App.js
+++ app/src/App.js
@@ -24,6 +24,7 @@ const App = () => {
           return (
               <div>
                   <PaymentPals drizzle={drizzle} drizzleState={drizzleState} />
+                  <WelcomeModal drizzle={drizzle} drizzleState={drizzleState} />
             </div>
           )
         }}
